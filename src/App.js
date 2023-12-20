9b0c01e10fc2ea27f95e761d4f5582aefb59a48b
Ravi MAlav <ravimalav05@gmail.com>
2023-12-20 19:26:48 +0530

    added header and cart button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./component/Layout/Header";
+import { Fragment } from "react";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/component/Layout/Header.js
@@ -0,0 +1,22 @@
+import Cart from "../Cart/CartIcon";
+import classes from "./Header.module.css";
+
+import { Fragment } from "react";
+
+import mealImage from "../../asset/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="A table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
