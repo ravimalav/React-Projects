6e972a330a0dff579faef51a3f8e36098e610528
Ravi MAlav <ravimalav05@gmail.com>
2024-01-18 17:52:58 +0530

    build navbar card and footer of website

---
+++ src/bootstrap-component/Footer.js
@@ -0,0 +1,11 @@
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
